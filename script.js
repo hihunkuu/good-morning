45530b6ca20c0ec7cea5104761a42724416bb25c
hihunkuu <hihunwopyuuu@gmail.com>
2025-02-02 03:48:15 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Animasi tambahan atau efek interaktif bisa ditambahkan di sini.
-// Misalnya, menampilkan pesan cinta setelah beberapa detik.
-window.onload = () => {
-    setTimeout(() => {
-        alert('Aku cinta padamu!');
-    }, 5000);
-};
